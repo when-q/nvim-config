7b9487134504fe7cc457598c3e64491cc650b417
when <when.discord@protonmail.com>
2022-12-23 23:38:41 +0000

    Colorscheme updates

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -26,11 +26,9 @@ vim.g['spring_night_high_contrast'] = 1
 vim.cmd[[
 augroup spring_override
 	autocmd!
-	  autocmd Colorscheme spring-night hi LineNr ctermfg=235 ctermbg=None guifg=None guibg=None
+	  autocmd Colorscheme spring-night hi LineNr ctermfg=231 ctermbg=231 guifg=#8d9eb2 guibg=#132132
 	  autocmd Colorscheme spring-night hi SignColumn ctermfg=None ctermbg=None guifg=None guibg=None
 augroup END
 ]]
-
 vim.api.nvim_command [[colorscheme spring-night]]
-
 vim.g['showtabline'] = 2
